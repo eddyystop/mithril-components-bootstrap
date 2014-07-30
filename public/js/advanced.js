0871830248e19396650ba79f085e67abebb706e4
eddyystop <johnsz@gmail.com>
2014-07-30 07:54:39 -0400

    m

--- public/js/advanced.js
+++ public/js/advanced.js
@@ -468,7 +468,7 @@ var componentPart = {
         ;
         break;
       case 'features':
-        var str = getFile (options.ex, '../dist/componentsUi.js/'  + options.doc);
+        var str = getFile (options.ex, '../dist/componentsUi.js');
         console.log('..............getFile', typeof str);
         //var str = getFile (options.ex, '../componentsUi/' + options.doc);
         var i = str.indexOf('## Controller');
